5f2bed3db012c755e9ada85080ed113d3356253c
Bjorn Ostby <bjorn.ostby@hiab.com>
2018-10-29 15:56:10 +0100

    ScriptGenerator: Check for source folder existense instead of hidden files

--- src/scriptGenerator.cc
+++ src/scriptGenerator.cc
@@ -41,7 +41,7 @@ void scriptGenerator::fetch(unique_ptr<firmwareRelease>& release, const string& 
 
     if (isWindows) {
         comment.assign("REM ");
-        script << "@echo off";
+        script << "@echo off" << endl;
         script << "IF NOT EXIST " << BUILDDIR << " md " << BUILDDIR << endl;
     } else {
         script << "#!/bin/sh" << std::endl;
@@ -62,16 +62,16 @@ void scriptGenerator::fetch(unique_ptr<firmwareRelease>& release, const string& 
             script << std::endl;
 
             if (isWindows) {
-                script << "IF NOT EXIST ." << entry->getRoot() << "-fetched (" << gitClone(entry) << " || exit 1)";
+                script << "IF NOT EXIST " << entry->getRoot() << " (" << gitClone(entry) << " || exit 1)";
                 script << " ELSE (@echo  **** Using local mirror)" << endl;
             } else {
-                script << "if [ -f ." << entry->getRoot() << "-fetched ]; then" << endl;
+                script << "if [ -f " << entry->getRoot() << " ]; then" << endl;
                 script << "echo  \" **** Using local mirror\"" << endl;
                 script << "else"  << endl;
                 script << gitClone(entry)  << "|| exit 1" << endl << "fi" << endl;
             }
 
-            script << "touch ." << entry->getRoot() << "-fetched" << endl;
+            //script << "touch " << BUILDDIR << PLT_SLASH << "." << entry->getRoot() << "-fetched" << endl;
             targetFetch = true;
         } else if (singleTarget.compare(entry->getName()) == 0) {
             // Fetch only one recipe component
@@ -86,11 +86,10 @@ void scriptGenerator::fetch(unique_ptr<firmwareRelease>& release, const string& 
             if (isWindows) {
                 script << "IF EXIST " << entry->getRoot() << " rm -rf " << entry->getRoot() << endl;
             } else {
-                cout << "removing!";
                 script << "rm -rf " << entry->getRoot() << endl;
             }
             script << gitClone(entry) << " || exit 1" << endl;
-            script << "touch ." << entry->getRoot() << "-fetched" << endl;
+            //script << "touch " << BUILDDIR << PLT_SLASH << "." << entry->getRoot() << "-fetched" << endl;
             targetFetch = true;
         }
     }
@@ -106,7 +105,7 @@ void scriptGenerator::build(unique_ptr<firmwareRelease>& release, const string& 
     std::ofstream script(SCRIPT_BUILD, std::ios_base::binary | std::ios_base::out);
 
     if (isWindows) {
-        script << "@echo off";
+        script << "@echo off" << endl;
         script << "IF NOT EXIST " << BUILDDIR << " md " << BUILDDIR << endl;
     } else {
         script << "#!/bin/sh" << std::endl;
@@ -135,7 +134,7 @@ void scriptGenerator::release(unique_ptr<firmwareRelease>& release)
     std::ofstream script(SCRIPT_RELEASE, std::ios_base::binary | std::ios_base::out);
 
     if (isWindows) {
-        script << "@echo off";
+        script << "@echo off" << endl;
         script << "IF NOT EXIST " << release->getReleasePath() << " md " << release->getReleasePath() << endl;
     } else {
         script << "#!/bin/sh" << std::endl;
