d58b0afe5302e309288faff74f2d169f59816f57
Bjorn Ostby <bjorn.ostby@hiab.com>
2018-11-14 17:31:22 +0100

    Let windows use uppercase for sources directory

--- src/kalyra.hh
+++ src/kalyra.hh
@@ -6,10 +6,12 @@
 #define KALYRA_BANNER "Kalyra Build System"
 
 #if defined(_WIN32) || defined(_WIN64)
+#define BUILDDIR "Sources"
 #define PLT_SUFFIX "bat"
 #define PLT_SHELL "cmd.exe  /C"
 #define PLT_SLASH "\\"
 #else
+#define BUILDDIR "sources"
 #define PLT_SUFFIX "sh"
 #define PLT_SHELL "bash"
 #define PLT_SLASH "/"
